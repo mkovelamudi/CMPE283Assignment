e76687c52cd1c71456fd80c33777f1e9bc186079
mkovelamudi <kovelamudi83@gmail.com>
2022-11-02 17:58:57 +0000

    Added rest of controls

--- Assignment1/cmpe283-1.c
+++ Assignment1/cmpe283-1.c
@@ -13,6 +13,9 @@
  */
 #define IA32_VMX_PINBASED_CTLS	        0x481
 #define IA32_VMX_PROCBASED_CTLS         0x482
+#define IA32_VMX_PROCBASED_CTLS2        0x48B
+#define IA32_VMX_EXIT_CTLS              0x483
+#define IA32_VMX_ENTRY_CTLS             0x484
 /*
  * struct caapability_info
  *
@@ -70,6 +73,86 @@ struct capability_info procbasedprimary[22] =
 };
 
 /*
+ProcBased controls secondary
+*See SDM Volume 3, section 24.6.2
+*/
+struct capability_info procbasedsecondary[27] =
+{
+        {0, "Virtualize APIC accesses"},
+        {1, "Enable EPT"},
+        {2, "Descriptor-table exiting"},
+        {3, "Enable RDTSCP"},
+        {4, "Virtualize x2APIC mode"},
+        {5, "Enable VPID"},
+        {6, "WBINVD exiting"},
+        {7, "Unrestricted guest"},
+        {8, "APIC-register virtualization"},
+        {9, "Virtual-interrupt delivery"},
+        {10, "PAUSE-loop exitin"},
+        {11, "RDRAND exiting"},
+        {12, "Enable INVPCID"},
+        {13, "Enable VM functions"},
+        {14, "VMCS shadowin"},
+        {15, "Enable ENCLS exiting"},
+        {16, "RDSEED exiting"},
+        {17, "Enable PML"},
+        {18, "EPT-violation #VE"},
+        {19, "Conceal VMX from PT"},
+        {20, "Enable XSAVES/XRSTORS"},
+        {22, "Mode-based execute control for EPT"},
+        {23, "Sub-page write permissions for EPT"},
+        {24, "Intel PT uses guest physical addresses"},
+        {25, "Use TSC scaling"},
+        {26, "Enable user wait and pause"},
+        {28, "Enable ENCLV exiting"}
+};
+
+/*
+*exit controls
+* See SDM Volume 3, section 24.7.1
+*/
+struct capability_info exitsbased[14] =
+{
+        {2, "Save debug controls"},
+        {9, "Host address-space size"},
+        {12, "Load IA32_PERF_GLOBAL_CTRL"},
+        {15, "Acknowledge interrupt on exit"},
+        {18, "Save IA32_PAT"},
+        {19, "Load IA32_PAT"},
+        {20, "Save IA32_EFER"},
+        {21, "Load IA32_EFER"},
+        {22, "Save VMX-preemption timer value"},
+        {23, "Clear IA32_BNDCFGS"},
+        {24, "Conceal VMX from PT"},
+        {25, "Clear IA32_RTIT_CT"},
+        {28, "Load CET state"},
+        {29, "Load PKRS"}
+
+};
+
+/*
+* vm entry controls
+* See SDM Volume 3, section 24.8.1
+*/
+
+struct capability_info entrybased[12] =
+{
+        {2, "Load debug controls"},
+        {9, "IA-32e mode guest"},
+        {10, "Entry to SMM"},
+        {11, "Deactivate dual-monitor treatment"},
+        {13, "Load IA32_PERF_GLOBAL_CTRL"},
+        {14, "Load IA32_PAT"},
+        {15, "Load IA32_EFER"},
+        {16, "Load IA32_BNDCFGS"},
+        {17, "Conceal VMX from PT"},
+        {18, "Load IA32_RTIT_CTL"},
+        {20, "Load CET state"},
+        {22, "Load PKRS"}
+
+};
+
+/*
  * report_capability
  *
  * Reports capabilities present in 'cap' using the corresponding MSR values
@@ -122,6 +205,24 @@ detect_vmx_features(void)
         pr_info("Procbased primary Controls MSR: 0x%llx\n",
                 (uint64_t)(lo | (uint64_t)hi << 32));
         report_capability(procbasedprimary, 22, lo, hi);
+
+	/* Procbased secondary controls */
+        rdmsr(IA32_VMX_PROCBASED_CTLS2, lo, hi);
+        pr_info("Procbased secondary Controls MSR: 0x%llx\n",
+                (uint64_t)(lo | (uint64_t)hi << 32));
+        report_capability(procbasedsecondary, 27, lo, hi);
+
+        /* exit controls */
+        rdmsr(IA32_VMX_EXIT_CTLS, lo, hi);
+        pr_info("Exit Controls MSR: 0x%llx\n",
+                (uint64_t)(lo | (uint64_t)hi << 32));
+        report_capability(exitsbased, 14, lo, hi);
+
+        /* entry controls */
+        rdmsr(IA32_VMX_ENTRY_CTLS, lo, hi);
+        pr_info("Entry Controls MSR: 0x%llx\n",
+                (uint64_t)(lo | (uint64_t)hi << 32));
+        report_capability(entrybased, 12, lo, hi);
 }
 
 /*
