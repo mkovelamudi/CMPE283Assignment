b5d091eeef349ea7687689596f9bfa4978e8454e
mkovelamudi <kovelamudi83@gmail.com>
2022-11-02 17:44:31 +0000

    Added Proc based Primary Controls

--- Assignment1/cmpe283-1.c
+++ Assignment1/cmpe283-1.c
@@ -11,8 +11,8 @@
  * Model specific registers (MSRs) by the module.
  * See SDM volume 4, section 2.1
  */
-#define IA32_VMX_PINBASED_CTLS	0x481
-
+#define IA32_VMX_PINBASED_CTLS	        0x481
+#define IA32_VMX_PROCBASED_CTLS         0x482
 /*
  * struct caapability_info
  *
@@ -36,6 +36,37 @@ struct capability_info pinbased[5] =
 	{ 5, "Virtual NMIs" },
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
+};
+
+/*
+*ProcBased controls primary
+*See SDM Volume 3, section 24.6.2
+*/
+struct capability_info procbasedprimary[22] =
+{
+        { 2, "Interrupt-window exiting" },
+        { 3, "Use TSC offsetting" },
+        { 7, "HLT exiting" },
+        { 9, "INVLPG exiting" },
+        { 10, "MWAIT exitin" },
+        { 11, "RDPMC exiting"},
+        { 12, "RDTSC exiting"},
+        { 15, "CR3-load exiting"},
+        { 16, "CR3-store exiting"},
+        { 17, "Activate tertiary controls"},
+        { 19, "CR8-load exiting"},
+        { 20, "CR8-store exiting"},
+        { 21, "Use TPR shadow"},
+        { 22, "NMI-window exiting"},
+        { 23, "MOV-DR exiting"},
+        { 24, "Unconditional I/O exiting"},
+        { 25, "Use I/O bitmaps"},
+        { 27, "Monitor trap flag"},
+        { 28, "Use MSR bitmaps"},
+        { 29, "MONITOR exiting"},
+        { 30, "PAUSE exiting"},
+        { 31, "Activate secondary controls"}
+
 };
 
 /*
@@ -85,6 +116,12 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
+
+	/* Procbased primary controls */
+        rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
+        pr_info("Procbased primary Controls MSR: 0x%llx\n",
+                (uint64_t)(lo | (uint64_t)hi << 32));
+        report_capability(procbasedprimary, 22, lo, hi);
 }
 
 /*
